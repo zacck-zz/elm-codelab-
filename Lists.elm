244ef9e8fe7a60757dbdff385feb5e96aa72bd42
zacck <coderv63@gmail.com>
2017-08-23 17:07:17 +0200

    apply formating to list learn how to iterate over lists and reduce them

--- Lists.elm
+++ Lists.elm
@@ -1,28 +1,62 @@
 module Lists exposing (..)
 
 import Html exposing (text)
--- for map functions 
-import List 
+
+
+-- for map functions
+
+import List
 
 
 -- create an alias for person type
-type alias Person = 
+
+
+type alias Person =
     { name : String
     , age : Int
     }
 
+
+
 -- populate a list of people
-people = 
-    [ { name  = "Carla", age = 27 }
+
+
+people =
+    [ { name = "Carla", age = 27 }
     , { name = "Zacck", age = 27 }
     ]
 
 
 
--- map through list of people and return list of names 
-names: List Person -> List String
-names peeps =  List.map (\peep -> peep.name) peeps 
+-- find a person in the list
 
 
-main = 
-    text <| toString <| names people
+findPerson : String -> List Person -> Maybe Person
+findPerson name peeps =
+    List.foldl
+        (\peep memo ->
+            case memo of
+                Just _ ->
+                    memo
+
+                Nothing ->
+                    if peep.name == name then
+                        Just peep
+                    else
+                        Nothing
+        )
+        Nothing
+        peeps
+
+
+
+-- map through list of people and return list of names
+
+
+names : List Person -> List String
+names peeps =
+    List.map (\peep -> peep.name) peeps
+
+
+main =
+    text <| toString <| findPerson "Zcck" people
